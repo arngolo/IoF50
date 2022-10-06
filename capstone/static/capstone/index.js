10881bc99fe22d2022dc676bf11850e216a04ef6
arngolo <arngolo@outlook.com>
2022-10-06 13:38:57 +0900

    exempt csrf and PUT fetched data into API url

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -4,8 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   // var ee = require('@google/earthengine');
   // console.log(ee.Image('LANDSAT/LC08/C01/T1/LC08_044034_20140318'));
   document.querySelector('#get_remote_image').addEventListener('click', () => push_pixels(array));
-  //   // document.querySelector('#username').addEventListener('click', () => display_posts(author, 'username_posts'));
-  //   // document.querySelector('#following').addEventListener('click', () => display_posts(author, 'following_posts'));
   });
 
 function push_pixels(array) {
@@ -13,7 +11,6 @@ function push_pixels(array) {
     method: 'PUT',
     body: JSON.stringify({
       remote_image:array,
-      credentials: 'same-origin',
     })
   });
 }
--- capstone/views.py
+++ capstone/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Imagery
 import json
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 def index(request):
-
      if not Imagery.objects.all():
 
           image = Imagery()
@@ -12,17 +13,25 @@ def index(request):
 
      return render(request, 'capstone/index.html')
 
+@csrf_exempt
 def pixels_app(request):
      if request.method == "GET":
           database = Imagery.objects.all()
-          images = [image for image in database.all()] ## all posts
+          images = [image for image in database.all()] ## all images
           # print(images)
 
           return JsonResponse([image.serialize() for image in images], safe=False)
 
      elif request.method == "PUT":
-          # for this project, the database needs to have at least 1 element. index function creates 1 by default.          
+          # for this project, the database needs to have at least 1 element. index function creates 1 by default.
+          image_update = Imagery.objects.get(pk=1)
           fetched_data = json.loads(request.body) #gets json data from the webpage (body as refered in javascript code). javascript uses "PUT" method of fetch to update the webpage contant
+          remote_image = fetched_data.get("remote_image")
+          image_update.remote_image=remote_image
           print(fetched_data.get("remote_image"))
+          image_update.save()
 
-
+          ## expose updated data again to the url
+          database = Imagery.objects.all()
+          images = [image for image in database.all()]
+          return JsonResponse([image.serialize() for image in images], safe=False)
