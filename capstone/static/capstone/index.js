d7b570a0a9807eaa7dde4ed368b02965ab66003b
arngolo <arngolo@outlook.com>
2022-10-20 00:34:10 +0900

    added leaflet map, overlayed personal maptile from google cloud storage

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const image_name = "LANDSAT/LC09/C02/T1/LC09_182066_20220611";
+
+      // Create the map with 2 layers
+      var map = L.map('map').setView([0, 0], 3);
+      var globalLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'OpenStreetMap'
+      }).addTo(map),
+      localLayer = L.tileLayer('https://storage.googleapis.com/beyond_rgb/mapTilesForLeaflet/{z}/{x}/{y}');
+  
+      var groupLayer = L.layerGroup([globalLayer, localLayer]);
+  
+      groupLayer.addTo(map);
+
+    const image_name = "LANDSAT/LC09/C02/T1/LC09_182066_20220611";
 
   // var ee = require('@google/earthengine');
   // console.log(ee.Image('LANDSAT/LC08/C01/T1/LC08_044034_20140318'));
